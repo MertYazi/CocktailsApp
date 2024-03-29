56016701f8332cf0a9d49e93b92d94647c634e3b
mert <mertyazi44@hotmail.com>
2024-03-30 02:39:48 +0300

    Dependency Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.1")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     }
 }
 plugins {
@@ -9,6 +8,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
-    id("androidx.navigation.safeargs") version "2.7.7" apply false
     id("org.sonarqube") version "5.0.0.4638" apply false
+    id("androidx.navigation.safeargs") version "2.7.7" apply false
 }
